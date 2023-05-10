2191663019d082b94d8ab3673bd61c2783e50116
kmj0973 <kmj0973@naver.com>
2023-05-11 02:36:41 +0900

    Learning useState, useEffect
    Learning useState, useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,31 @@
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
+
+  useEffect(() => {
+    console.log("Movie search", keyword);
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>hello</h1>
-      <Button text={"continue"} />
+      <input
+        value={keyword}
+        type="text"
+        placeholder="Search here..."
+        onChange={onChange}
+      ></input>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={"continue"} onClick={onClick} />
     </div>
   );
 }
