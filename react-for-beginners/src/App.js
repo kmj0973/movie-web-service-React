40bf4d3d1d4b090f6cd4b32e506be4307827ff96
kmj0973 <kmj0973@naver.com>
2023-05-11 16:57:57 +0900

    Todolist using React-1
    Todolist using React-1

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,29 +3,29 @@ import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
-
-  useEffect(() => {
-    console.log("CALL THE API");
-  }, []);
-
-  useEffect(() => {
-    console.log("Movie search", keyword);
-  }, [keyword]);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange =(event) => setTodo(event.target.value);
+  const onSubmit =(event) => {
+    event.preventDefault()
+    if(todo === "") {
+      return;
+    }
+    setTodos(currentArray => [todo, ...currentArray]);
+    setTodo("");
+  }
+  console.log(todos);
   return (
     <div>
-      <input
-        value={keyword}
-        type="text"
-        placeholder="Search here..."
-        onChange={onChange}
-      ></input>
-      <h1 className={styles.title}>{counter}</h1>
-      <Button text={"continue"} onClick={onClick} />
+      <h1>My To Dos {todos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={todo} type="text" placeholder="Write your to do..."></input>
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      <ul>
+      {todos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
