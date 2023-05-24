a8a9bc9c57ead8c6857cc49b05dab040de072143
kmj0973 <kmj0973@naver.com>
2023-05-24 22:37:26 +0900

    update app
    update app

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,60 +1,40 @@
 import { useState, useEffect } from "react";
-import Button from "./Button";
-import styles from "./App.module.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [dollars, setDollars] = useState(0);
-  const [check, setCheck] = useState(true);
-  const [coin1, setCoin] = useState([]);
-
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-  const onChange = (event) => {
-    setDollars(event.target.value);
-    setCheck(false);
-  };
-
-  const onChoose = (event) => {
-    setCoin(coins.filter((current, index) => event.target.value == index));
-    console.log(event.target.value);
-  };
-
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      <input
-        type="text"
-        placeholder="Write your dollar"
-        value={dollars}
-        onChange={onChange}
-      ></input>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onChoose}>
-          <option>==== choose ====</option>
-          {coins.map((coin, index) => (
-            <option key={index} value={index}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
-            </option>
-          ))}
-        </select>
-      )}
-
-      <h1>
-        You can buy{" "}
         <div>
-          {check ? "Write your dollars" : dollars / coin1[0].quotes.USD.price}
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => {
+                  <li key={g}>{g}</li>;
+                })}
+              </ul>
+            </div>
+          ))}
         </div>
-      </h1>
+      )}
+      ;
     </div>
   );
 }
