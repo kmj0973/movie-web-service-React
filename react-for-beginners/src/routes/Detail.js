b10ec5c943a698db96311905b6a31fcbdc1d5cd1
kmj0973 <kmj0973@naver.com>
2023-05-29 20:00:06 +0900

    Update movie list-1
    Update movie list-1

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,5 +1,40 @@
+import { useEffect,useState } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
 function Detail(){
-    return <h1>hello detail</h1>
+    const [movie,setMovies] = useState([]);
+    const {id} = useParams();
+    const getMovie = async() =>{
+        const json = await(
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        console.log(json);
+        setMovies(json.data.movie)
+    }
+    console.log(movie);
+    useEffect(()=>{
+      getMovie();
+    }, []);
+    
+    return (
+        <div>
+            <div>
+        
+            {/* <Movie
+              key={movie.id}
+              id={movie.id}
+              mediumCoverImage={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            /> */}
+
+        </div>
+            <h1>{id}</h1>
+            <h2>hello</h2>
+        </div>
+    )
 }
 
 export default Detail;
--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -23,6 +23,7 @@ function Home() {
         <div>
           {movies.map((movie) => (
             <Movie
+              key={movie.id}
               id={movie.id}
               mediumCoverImage={movie.medium_cover_image}
               title={movie.title}
