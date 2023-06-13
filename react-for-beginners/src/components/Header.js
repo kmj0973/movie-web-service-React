cc820083bd86b94bb2a2b2910e50e51b73a53125
kmj0973 <kmj0973@naver.com>
2023-06-14 04:42:21 +0900

    Update movie-list12, header css
    Update movie-list12, header css

--- react-for-beginners/src/components/Header.js
+++ react-for-beginners/src/components/Header.js
@@ -1,47 +1,48 @@
-import { useState } from 'react';
-import styles from './Header.module.css';
-
+import { useState } from "react";
+import styles from "./Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 function Header() {
-    function onClickMenu() {
-        setMenu((menu) => !menu);
+  function onClickMenu() {
+    setMenu((menu) => !menu);
 
-        console.log(menu);
-    }
-    const [menu, setMenu] = useState(false);
-    return (
-        <header className={styles.header}>
-            <div className={styles.header__title}>
-                <div>NETFLIX</div>
-            </div>
+    console.log(menu);
+  }
+  const [menu, setMenu] = useState(false);
+  return (
+    <nav className={styles.header}>
+      <div className={styles.header__title}>
+        <div>NETFLIX</div>
+      </div>
 
-            <ul className={menu ? styles.header__options : styles.none}>
-                <li>
-                    <a href="">Romance</a>
-                </li>
-                <li>
-                    <a href="">Thriller</a>
-                </li>
-                <li>
-                    <a href="">Action</a>
-                </li>
-            </ul>
-            <ul className={menu ? styles.header__about_us : styles.none}>
-                <li>twitter</li>
-                <li>instagram</li>
-            </ul>
-            {/* {menu ? (
-        ""
-      ) : (
-        <div className={(styles.header__about_us, styles.none)}>
-          <div>twitter</div>
-          <div>instagram</div>
-        </div>
-      )} */}
-            <a href="#" className={styles.header__menu} onClick={onClickMenu}>
-                menu
-            </a>
-        </header>
-    );
+      <ul className={`${styles.header__options} ${menu ? styles.flex : ""}`}>
+        <li>
+          <a href="#">Romance</a>
+        </li>
+        <li>
+          <a href="#">Thriller</a>
+        </li>
+        <li>
+          <a href="#">Action</a>
+        </li>
+      </ul>
+
+      <ul className={`${styles.header__about_us} ${menu ? styles.flex : ""}`}>
+        <li>
+          <FontAwesomeIcon icon={faTwitter} />
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faInstagram} />
+        </li>
+      </ul>
+
+      <a href="#" className={styles.header__menu} onClick={onClickMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </a>
+    </nav>
+  );
 }
 
 export default Header;
